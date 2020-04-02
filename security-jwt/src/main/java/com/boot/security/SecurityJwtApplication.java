de4748b253dded4d5795ea54810a41056e973cae
zhangshuai921 <13121231257@163.com>
2020-04-02 14:06:18 +0800

    添加 @MapperScan注解，扫描dao层

--- security-jwt/src/main/java/com/boot/security/SecurityJwtApplication.java
+++ security-jwt/src/main/java/com/boot/security/SecurityJwtApplication.java
@@ -1,9 +1,11 @@
 package com.boot.security;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.boot.security.mapper")
 public class SecurityJwtApplication {
 
     public static void main(String[] args) {
